8b86e9e6bf3f6d32ac9367aace7d4e44596401ac
Andrea <anvil1@fiv.upv.es>
2012-03-12 20:14:47 +0100

    [PATCH] GENERA_LIBTOR_BOT.sh
    "echo" en lugar de "print" xD

--- libtor/tor-0.2.2.35/GENERA_LIBTOR_BOT.sh
+++ libtor/tor-0.2.2.35/GENERA_LIBTOR_BOT.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-print "En que puerto quieres que escuche tor? "
+echo "En que puerto quieres que escuche tor? "
 read PUERTO_TOR
 if [ ! -z $PUERTO_TOR ]; then
 	sed -r "s/BOT_SOCKSPORT [0-9]+/BOT_SOCKSPORT $PUERTO_TOR/" src/or/config.c > src/or/config_new.c
